d780c020fec47c9cf8fe2bfe398727747f2b69e4
Jamie Macey <jamie.git@tracefunc.com>
2009-01-13 09:26:51 -0500

    answer 1.6, frame solution for 1.7

---
+++ ch_1/ex1_7.clj
@@ -0,0 +1,48 @@
+(defn abs [x]
+  (cond (< x 0) (- x)
+        :else x))
+
+(defn square [x]
+  (* x x))
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn good-enough? [guess x]
+  (< (abs (- (square guess) x)) 0.001))
+
+(defn sqrt-iter [guess x]
+  (if (good-enough? guess x)
+      guess
+      (recur (improve guess x)
+             x)))
+
+(defn sqrt [x]
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
+(sqrt (+ 100 37))
+(sqrt (+ (sqrt 2) (sqrt 3)))
+(square (sqrt 1000))
+
+; In very small numbers, the delta used in good-enough? can be many times
+; the result we want, leading to inaccurate answers.
+
+(sqrt (square 0.000000001))
+; Gives us 0.03125000000000001, because it gives up too early
+
+; In very large numbers, (for example, adding a digit to the below) the
+; floating-point representation is incapable of becoming any more accurate
+; with each iteration (average x y returning x) and so we get into an
+; infinite loop without making any progress
+(sqrt (square 1234567890987654))
+
+(defn good-enough? [guess x]
+  (< (abs (- (square guess) x)) 0.001))
+
+(sqrt (square 0.000000001))
+(sqrt (square 1234567890987654))
+;(sqrt (square 12345678909876543))
