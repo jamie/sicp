a3899c33684ccc74ea787b2a3b9e5d3356bb4d16
Jamie Macey <jamie.git@tracefunc.com>
2009-01-22 21:54:45 -0500

    questions for sections 1.2

---
+++ 1.2.2/1.11.clj
@@ -0,0 +1,3 @@
+; A function f is defined by the rule that f(n) = n if n<3 and
+; f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n>= 3. Write a procedure that
+; computes f computes f by means of an iterative process. 
