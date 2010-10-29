6aeb1a13ca89b6df1237b7fc3a6bb87837ad985e
Jamie Macey <jamie@tracefunc.com>
2010-10-28 21:14:20 -0700

    comment format

--- 1.2.6/1.21.clj
+++ 1.2.6/1.21.clj
@@ -1,4 +1,22 @@
 ; Use the smallest-divisor procedure to find the smallest divisor of each of
 ; the following numbers: 199, 1999, 19999.
 
-; TODO
+(defn square [x]
+  (* x x))
+(defn remainder [x y]
+  (if (< x y)
+      x
+      (recur (- x y) y)))
+
+(defn smallest-divisor [n]
+  (defn divides? [a b]
+    (= remainder b a) 0)
+  (defn find-divisor [n test]
+    (cond (> (square test) n) n
+          (divides? test n) test
+          :else (find-divisor n (+ test 1))))
+  (find-divisor n 2))
+
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
--- 1.2.6/1.22.clj
+++ 1.2.6/1.22.clj
@@ -1,4 +1,9 @@
-; Most Lisp implementations include a primitive called runtime that returns an integer that specifies the amount of time the system has been running (measured, for example, in microseconds). The following timed-prime-test procedure, when called with an integer n, prints n and checks to see if n is prime. If n is prime, the procedure prints three asterisks followed by the amount of time used in performing the test.
+; Most Lisp implementations include a primitive called runtime that returns an
+; integer that specifies the amount of time the system has been running
+; (measured, for example, in microseconds). The following timed-prime-test
+; procedure, when called with an integer n, prints n and checks to see if n is
+; prime. If n is prime, the procedure prints three asterisks followed by the
+; amount of time used in performing the test.
 ;
 ;    (define (timed-prime-test n)
 ;      (newline)
